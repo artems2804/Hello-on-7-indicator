21abd26614cc4fa65eb610e5a6cfd5af188176bf
ar2 <artemseov2804@gmail.com>
2024-10-27 11:02:27 +0300

    show

---
+++ Ardino_7_indicator_HELLO/Ardino_7_indicator_HELLO.ino
@@ -0,0 +1,73 @@
+int i = 8;
+int a = 2;
+int b = 3;
+int c = 4;
+int d = 5;
+int e = 6;
+int f = 7;
+int g = 8;
+int point = 9;
+int o = 8;
+void setup() {
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(point, OUTPUT);
+}
+
+void loop() {
+ digitalWrite(a, LOW);
+ digitalWrite(b, HIGH);
+ digitalWrite(c, HIGH);
+ digitalWrite(d, LOW);
+ digitalWrite(e, HIGH);
+ digitalWrite(f, HIGH);
+ digitalWrite(g, HIGH);
+ digitalWrite(point, LOW);
+ delay(500);
+ digitalWrite(a, HIGH);
+ digitalWrite(b, LOW);
+ digitalWrite(c, LOW);
+ digitalWrite(d, HIGH);
+ digitalWrite(e, HIGH);
+ digitalWrite(f, HIGH);
+ digitalWrite(g, HIGH);
+ digitalWrite(point, LOW);
+ delay(500);
+ digitalWrite(a, LOW);
+ digitalWrite(b, LOW);
+ digitalWrite(c, LOW);
+ digitalWrite(d, HIGH);
+ digitalWrite(e, HIGH);
+ digitalWrite(f, HIGH);
+ digitalWrite(g, LOW);
+ digitalWrite(point, LOW);
+ delay(500);
+ digitalWrite(a, LOW);
+ digitalWrite(b, LOW);
+ digitalWrite(c, LOW);
+ digitalWrite(d, HIGH);
+ digitalWrite(e, HIGH);
+ digitalWrite(f, HIGH);
+ digitalWrite(g, LOW);
+ digitalWrite(point, LOW);
+ delay(500);
+ digitalWrite(a, HIGH);
+ digitalWrite(b, HIGH);
+ digitalWrite(c, HIGH);
+ digitalWrite(d, HIGH);
+ digitalWrite(e, HIGH);
+ digitalWrite(f, HIGH);
+ digitalWrite(g, LOW);
+ digitalWrite(point, LOW);
+ for (o; o<0; o--){
+  
+ }
+ delay(500);
+  o = 4
+
+}
